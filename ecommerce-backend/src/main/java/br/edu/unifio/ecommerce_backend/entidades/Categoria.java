6c221eb0b64515c359bdf66ce18d04c96a419c7e
LUIZ FELIPE GASPAR FERNANDES DA SILVA <273435@unifio.edu.br>
2026-08-19 01:41:25 +0000

    Configuração do JPA

---
+++ ecommerce-backend/src/main/java/br/edu/unifio/ecommerce_backend/entidades/Categoria.java
@@ -0,0 +1,14 @@
+package br.edu.unifio.ecommerce_backend.entidades;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Categoria {
+    private short id;
+    private String nome;
+    private String descricao;
+
+    
+}
