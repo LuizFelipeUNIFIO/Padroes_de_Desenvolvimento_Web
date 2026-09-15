b5edbe37d74171075734a1e29de5c041b80b427b
LUIZ FELIPE GASPAR FERNANDES DA SILVA <273435@unifio.edu.br>
2026-09-15 23:39:37 +0000

    Aula do Dia 15/09/2026

--- ecommerce-backend/src/test/java/br/edu/unifio/ecommerce_backend/repositorios/ProdutoRepositorioTests.java
+++ ecommerce-backend/src/test/java/br/edu/unifio/ecommerce_backend/repositorios/ProdutoRepositorioTests.java
@@ -1,13 +1,17 @@
 package br.edu.unifio.ecommerce_backend.repositorios;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
 
 import br.edu.unifio.ecommerce_backend.entidades.Categoria;
 import br.edu.unifio.ecommerce_backend.entidades.Produto;
@@ -35,5 +39,59 @@ public class ProdutoRepositorioTests {
 
         assertNotNull(produto.getId());
         assertEquals(6, produto.getId());
+    }
+
+    @Test
+    public void deveBuscarUmProduto() {
+        Produto produto = produtoRepositorio.findById(Integer.parseInt("3")).orElseThrow();
+        assertNotNull(produto);
+        assertEquals("Poco X8 Pro", produto.getNome());
+    }
+
+    @Test
+    public void deveBuscarTodosOsProdutos() {
+        List<Produto> produtos = produtoRepositorio.findAll(Sort.by("nome") );
+
+        assertEquals(7, produtos.size());
+        assertEquals("Código Limpo", produtos.get(0).getNome());
+
+    }
+
+    @Test 
+    public void deveExcluirUmProdutoPorId() {
+        var produto = new Produto();
+        produto.setNome("Nome Teste");
+        produto.setDescricao("Teste");
+        produto.setPreco(new BigDecimal("00.00"));
+        produto.setEstoque(Short.parseShort("0"));
+        
+        Categoria categoria = categoriaRepositorio.findById(Short.parseShort("1")).orElseThrow();
+        produto.setCategoria(categoria);
+
+        produtoRepositorio.save(produto);
+
+        assertTrue(produtoRepositorio.existsById(produto.getId()));
+        produtoRepositorio.deleteById(produto.getId());
+        assertFalse(produtoRepositorio.existsById(produto.getId()));
+    }
+
+    @Test
+    public void deveAtualizarONomeDeUmProduto() {
+        var produto = new Produto();
+        produto.setNome("Nome Teste");
+        produto.setDescricao("Teste");
+        produto.setPreco(new BigDecimal("00.00"));
+        produto.setEstoque(Short.parseShort("0"));
+        
+        Categoria categoria = categoriaRepositorio.findById(Short.parseShort("1")).orElseThrow();
+        produto.setCategoria(categoria);
+
+        produtoRepositorio.save(produto);
+
+        produto.setNome("Outro Nome Teste");
+
+        produtoRepositorio.save(produto);
+
+        assertEquals("Outro Nome Teste", produtoRepositorio.findById(produto.getId()).orElseThrow().getNome());
     }
 }
